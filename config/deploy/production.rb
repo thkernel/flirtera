f7f95795b6c3b9881356caacad1af9299345be65
thkernel <salut.amos@gmail.com>
2020-06-29 18:59:04 +0000

    update

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,10 +7,10 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 set :application, "flirtera.net"               # the name of your website - should also be the name of the directory
-set :deploy_to, "/home/ubuntu/rails-apps/flirtera.prod"   # the path to your new deployment directory on the server - by default, the name of the application (e.g. "/var/www/sites/example.com")
+set :deploy_to, "/home/ubuntu/apps/rails/flirtera/flirtera.production"   # the path to your new deployment directory on the server - by default, the name of the application (e.g. "/var/www/sites/example.com")
 set :rails_env, "production"
 
-server "51.38.235.131", user: "root", roles: %w{app db web}, ssh_options: {forward_agent: true}
+server "51.89.167.78", user: "ubuntu", roles: %w{app db web}, ssh_options: {forward_agent: true}
 
 set :branch, "master"
 set :port, 3003
@@ -18,8 +18,8 @@ set :port, 3003
 
 
 
- set :nginx_config_name, "flirtera.prod"
- set :nginx_server_name, "flirtera.prod"
+ set :nginx_config_name, "flirtera.production"
+ set :nginx_server_name, "flirtera.production"
  set :puma_workers, 1
 
 # role-based syntax
---
+++ config/initializers/constantes.rb
@@ -0,0 +1,1 @@
+GENDER = [["Femme"], ["Homme"]]
